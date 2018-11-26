220ba606e463dcdcd4081d2742a2afa84fc41a4d
David Cole <dcole@inviqa.com>
2018-11-26 13:09:49 +0000

    misc improvements

--- docker/image/console/root/lib/task/drupal/install.sh
+++ docker/image/console/root/lib/task/drupal/install.sh
@@ -8,10 +8,10 @@ function task_drupal_install()
         --langcode=en \
         \
         --db-type=mysql \
-        --db-host=mysql \
-        --db-name=drupal \
-        --db-user=drupal \
-        --db-pass=drupal \
+        --db-host=${DB_HOST} \
+        --db-name=${DB_NAME} \
+        --db-user=${DB_USER} \
+        --db-pass=${DB_PASS} \
         --db-port=3306 \
         \
         --site-name=Drupal \
@@ -20,6 +20,9 @@ function task_drupal_install()
         --account-name=admin \
         --account-mail=admin@example.com \
         --account-pass=admin123"
+
+    chmod 755 /app/web/sites/default
+    rm -f /app/web/sites/default/settings.php
 
     task "skeleton:apply"
     task "assets:dump"
