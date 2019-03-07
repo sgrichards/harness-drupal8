d399bb580ff8a07852c3780c4524a8852f353177
dcole-inviqa <dcole@inviqa.com>
2019-03-07 18:58:18 +0000

    rework build and init steps

--- docker/image/console/root/lib/task/build.sh
+++ docker/image/console/root/lib/task/build.sh
@@ -2,23 +2,8 @@
 
 function task_build()
 {
-    case "$1" in
-        "pass-1")
-            _build_pass_1
-            ;;
-        "pass-2")
-            _build_pass_2
-            ;;
-    esac
-}
-
-_build_pass_1()
-{
     task "skeleton:apply"
-    task "composer:install"
-}
 
-_build_pass_2()
-{
-    echo "Pass 2"
+    task "build:backend"
+    task "build:frontend"
 }
--- docker/image/console/root/lib/task/drupal/refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-function task_drupal_refresh()
-{
-    run "drupal cc"
-}
